3c3368c4b9d84aa4f29e3500745f14a4b4a21840
olayode <aolayode@getcarbon.co>
2022-02-25 20:41:27 +0100

    implement kafka consumer

--- src/main/java/com/possible/notification_kafka/NotificationKafkaApplication.java
+++ src/main/java/com/possible/notification_kafka/NotificationKafkaApplication.java
@@ -1,13 +1,21 @@
 package com.possible.notification_kafka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class NotificationKafkaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationKafkaApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(KafkaTemplate<String, String> kafkaTemplate){
+        return args -> kafkaTemplate.send("FSS", "Testing Testing Hello");
     }
 
 }
