8fdaef9c5cf27eb39307e4c3608c7c46379a0c5c
olayode <aolayode@getcarbon.co>
2022-02-25 20:05:07 +0100

    configure kafka producer

--- src/main/java/com/possible/notification_kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/possible/notification_kafka/config/KafkaProducerConfig.java
@@ -3,7 +3,11 @@ package com.possible.notification_kafka.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,5 +25,15 @@ public class KafkaProducerConfig {
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return properties;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
     }
 }
